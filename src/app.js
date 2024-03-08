0bd9753d91459dde42e5f2994b23710f6356cee0
jovahnsc <jovanshann@gmail.com>
2024-03-08 16:55:33 +0700

    Finish All UI

--- src/app.js
+++ src/app.js
@@ -27,3 +27,33 @@ const scrollUp = () => {
   }
 };
 window.addEventListener("scroll", scrollUp);
+
+// Dark Mode
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("mode") == "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+  if (localStorage.getItem("mode") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
+}
