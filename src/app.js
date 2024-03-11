c386d6684e6a2daef7328defb481648694735c72
jovahnsc <jovanshann@gmail.com>
2024-03-11 19:19:48 +0700

    Penambahan Animation

--- src/app.js
+++ src/app.js
@@ -57,3 +57,67 @@ function lightMode() {
   themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
   localStorage.setItem("mode", "light");
 }
+
+// Scroll Sections Active Link
+const activeLink = () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  let current = "home";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (this.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((item) => {
+    item.classList.remove("text-secondaryColor");
+    if (item.href.includes(current)) {
+      item.classList.add("text-secondaryColor");
+    }
+  });
+};
+
+window.addEventListener("scroll", activeLink);
+
+// Scroll Reveal
+Window.sr = ScrollReveal({ reset: true });
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".home-image", {
+  origin: "right",
+});
+sr.reveal(".home-content", {
+  origin: "left",
+});
+
+sr.reveal(".new-content");
+sr.reveal(".new-image-left", {
+  origin: "left",
+});
+sr.reveal(".new-image-right", {
+  origin: "right",
+});
+
+sr.reveal(".bestseller-left");
+sr.reveal(".bestseller-right", { origin: "bottom" });
+
+sr.reveal(".category");
+
+sr.reveal(".review", { origin: "bottom" });
+
+sr.reveal(".about-right", {
+  origin: "left",
+});
+sr.reveal(".about-left", {
+  origin: "right",
+});
